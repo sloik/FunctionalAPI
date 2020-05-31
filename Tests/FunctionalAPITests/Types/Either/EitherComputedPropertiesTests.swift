2d39ce1be0f219b67fea3508d9eb3dfd2dff0e64
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-05-31 10:26:07 +0200

    Adds tests.

--- Tests/FunctionalAPITests/Types/Either/EitherComputedPropertiesTests.swift
+++ Tests/FunctionalAPITests/Types/Either/EitherComputedPropertiesTests.swift
@@ -84,6 +84,97 @@ class EitherComputedPropertiesTests: XCTestCase {
     }
     
     // MARK: - Maps
+    func test_map() {
+        XCTAssertEqual(right.map(identity), right)
+        XCTAssertTrue (right.map(increment).isRight)
+        XCTAssertEqual(right.map(increment).right, 43)
+        
+        XCTAssertEqual(left.map(identity), left)
+        XCTAssertTrue (left.map(increment).isLeft)
+        XCTAssertEqual(left.map(increment).left, "I'm left")
+    }
+    
+    func test_free_map() {
+        XCTAssertEqual(map(right, identity), right)
+        XCTAssertTrue (map(right, increment).isRight)
+        XCTAssertEqual(map(right, increment).right, 43)
+        
+        XCTAssertEqual(map(left, identity), left)
+        XCTAssertTrue (map(left, increment).isLeft)
+        XCTAssertEqual(map(left, increment).left, "I'm left")
+    }
+    
+    func test_mapRight() {
+        XCTAssertEqual(right.mapRight(identity), right)
+        XCTAssertTrue (right.mapRight(increment).isRight)
+        XCTAssertEqual(right.mapRight(increment).right, 43)
+        
+        XCTAssertEqual(left.mapRight(identity), left)
+        XCTAssertTrue (left.mapRight(increment).isLeft)
+        XCTAssertEqual(left.mapRight(increment).left, "I'm left")
+    }
+    
+    func test_free_mapRight() {
+        XCTAssertEqual(mapRight(right, identity), right)
+        XCTAssertTrue (mapRight(right, increment).isRight)
+        XCTAssertEqual(mapRight(right, increment).right, 43)
+        
+        XCTAssertEqual(mapRight(left, identity), left)
+        XCTAssertTrue (mapRight(left, increment).isLeft)
+        XCTAssertEqual(mapRight(left, increment).left, "I'm left")
+    }
+    
+    func test_mapLeft() {
+        XCTAssertEqual(right.mapLeft(identity), right)
+        XCTAssertTrue (right.mapLeft({ $0.uppercased() }).isRight)
+        XCTAssertEqual(right.mapLeft({ $0.uppercased() }).right, 42)
+        
+        XCTAssertEqual(left.mapLeft(identity), left)
+        XCTAssertTrue (left.mapLeft({ $0.uppercased() }).isLeft)
+        XCTAssertEqual(left.mapLeft({ $0.uppercased() }).left, "I'M LEFT")
+    }
+    
+    func test_free_mapLeft() {
+        XCTAssertEqual(mapLeft(right, identity), right)
+        XCTAssertTrue (mapLeft(right, { $0.uppercased() }).isRight)
+        XCTAssertEqual(mapLeft(right, { $0.uppercased() }).right, 42)
+        
+        XCTAssertEqual(mapLeft(left, identity), left)
+        XCTAssertTrue (mapLeft(left, { $0.uppercased() }).isLeft)
+        XCTAssertEqual(mapLeft(left, { $0.uppercased() }).left, "I'M LEFT")
+    }
+    
+    func test_mapBi() {
+        XCTAssertEqual(right.mapBi(identity, identity), right)
+        XCTAssertEqual(left.mapBi(identity, identity), left)
+        
+        XCTAssertEqual(
+            right.mapBi({ $0.uppercased() }, increment),
+            .right(43)
+        )
+        
+        XCTAssertEqual(
+            left.mapBi({ $0.uppercased() }, increment),
+            .left("I'M LEFT")
+        )
+    }
+    
+    func test_free_mapBi() {
+        XCTAssertEqual(mapBi(right, identity, identity), right)
+        XCTAssertEqual(mapBi(left, identity, identity), left)
+        
+        XCTAssertEqual(
+            mapBi(right, { $0.uppercased() }, increment),
+            .right(43)
+        )
+        
+        XCTAssertEqual(
+            mapBi(left, { $0.uppercased() }, increment),
+            .left("I'M LEFT")
+        )
+    }
+    
+    // MARK: Flat Maps
 }
 
 extension String: Error {}
