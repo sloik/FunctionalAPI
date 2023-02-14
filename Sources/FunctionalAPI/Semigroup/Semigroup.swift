20af47023cef9c2983d1692a7efa6cdab16340c4
Lukasz <stocki.lukasz@gmail.com>
2023-02-15 00:23:29 +0100

    Updates documentation.

--- Sources/FunctionalAPI/Semigroup/Semigroup.swift
+++ Sources/FunctionalAPI/Semigroup/Semigroup.swift
@@ -9,6 +9,13 @@ infix operator <>: SemigroupPrecedence
 /// A semigroup is a type that supports a binary operation (`combine`) that is
 /// associative.
 ///
+/// An associative operation is an operation that can be applied
+/// to three or more values in any order, and the result will always be the same.
+/// In other words, if you have three values a, b, and c, then (a * b) * c will
+/// always be equal to a * (b * c) if the operation * is associative. This property
+/// is important in mathematics and computer science because it allows us to group
+/// operations in different ways without changing the result.
+///
 /// Some types can have more than one valid implementation of the `Semigroup`
 /// protocol, and the witness passed to the `combine` method determines which
 /// implementation to use.
